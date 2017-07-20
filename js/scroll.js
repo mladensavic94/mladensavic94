2b48800bc4ad335c99b64b736d4c84982729c166
Mladen Savic <mladensavic94@gmail.com>
2017-07-20 17:45:35 +0200

    dodata animacija za scroll

--- js/scroll.js
+++ js/scroll.js
@@ -1,11 +1,7 @@
-$('a[href*="#"]')
 
-// Remove links that don't actually link to anything
-    .not('[href="#"]')
-    .not('[href="#0"]')
-    .click(function(event) {
-alert("ASD");
+$(document).on('click', 'a', function(event) {
         // On-page links
+    event.preventDefault();
         if (
             location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
             &&
@@ -20,17 +16,8 @@ alert("ASD");
                 event.preventDefault();
                 $('html, body').animate({
                     scrollTop: target.offset().top
-                }, 1000, function() {
-                    // Callback after animation
-                    // Must change focus!
-                    var $target = $(target);
-                    $target.focus();
-                    if ($target.is(":focus")) { // Checking if the target was focused
-                        return false;
-                    } else {
-                        $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-                        $target.focus(); // Set focus again
-                    }
+                }, 750, function() {
+
                 });
             }
         }
